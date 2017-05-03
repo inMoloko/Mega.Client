6ec344cbf325cf8c7ed00c29ab7edbb32d77ea5b
nekrasovinmoloko <pitchcontrol@yandex.ru>
2017-05-03 10:06:49 +0500

    Отправка обратной связи

--- Scripts/Services/feedbackService.js
+++ Scripts/Services/feedbackService.js
@@ -8,10 +8,10 @@
         var self = this;
         return self._$http.post(this._settings.webApiBaseUrl + '/Feedback/?CustomerID=' + self._settings.customerID,
             {
-                FeedbackCommentText: feedback.text + '\nОценка: ' + feedback.rating,
+                FeedbackCommentText: feedback.text,
                 ApplicantName: 'MEGA client',
-                FeedbackMark: feedback.rating > 3 ? "good" : "bad",
-                SelectedFeedbackCategory:1
+                FeedbackMark: feedback.rating,
+                SelectedFeedbackCategory:-1
             });
     };
     service.$inject = ['$rootScope', '$http', 'settings'];
