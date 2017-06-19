2d36fb8b9fea9f2eb7587841d837137df52a53db
nekrasovinmoloko <pitchcontrol@yandex.ru>
2017-06-19 14:01:27 +0500

    Теперь отзыв сохраняется в как и статистика в IndexDB

--- Scripts/Services/feedbackService.js
+++ Scripts/Services/feedbackService.js
@@ -1,19 +1,33 @@
 ﻿(function () {
     'use strict';
-    var service = function ($rootScope, $http, settings) {
+    var service = function ($rootScope, $http, settings, $q, statisticService) {
         this._$http = $http;
         this._settings = settings;
+        this._$q = $q;
+        this._statisticService = statisticService;
     };
     service.prototype.post = function (feedback) {
-        var self = this;
-        return self._$http.post(this._settings.webApiBaseUrl + '/Feedback/?CustomerID=' + self._settings.customerID,
-            {
-                FeedbackCommentText: feedback.text,
-                ApplicantName: 'MEGA client',
-                FeedbackMark: feedback.rating,
-                SelectedFeedbackCategory:-1
-            });
+        let self = this;
+        // return self._$http.post(this._settings.webApiBaseUrl + '/Feedback/?CustomerID=' + self._settings.customerID,
+        //     {
+        //         FeedbackCommentText: feedback.text,
+        //         ApplicantName: 'MEGA client',
+        //         FeedbackMark: feedback.rating,
+        //         SelectedFeedbackCategory:-1
+        //     });
+        let obj = {
+            FeedbackCommentText: feedback.text,
+            ApplicantName: 'MEGA client',
+            FeedbackMark: feedback.rating,
+            SelectedFeedbackCategory: -1
+        };
+        self._statisticService.addStatistic({
+            Action: 'SendCommentCommand',
+            ParamsAsJson: obj,
+            Date: new Date()
+        });
+        return self._$q.resolve(obj);
     };
-    service.$inject = ['$rootScope', '$http', 'settings'];
+    service.$inject = ['$rootScope', '$http', 'settings', '$q', 'statisticService'];
     angular.module('app').service('feedbackService', service);
 })();
