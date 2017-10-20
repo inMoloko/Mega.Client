3f4c65dba1c5d14e268348ac48f83d669ddd1d60
nekrasov <nekrasov@inmoloko.ru>
2017-10-20 16:34:20 +0500

    Ограничение на длинну строки в названиях

--- Scripts/Services/dbService.js
+++ Scripts/Services/dbService.js
@@ -15,6 +15,15 @@
             this.$linq = $linq;
         }
 
+        _setRootCategories(data, name) {
+            let obj = data.SystemSettings.TERMINAL_MENU_ITEMS[name];
+            obj = data.Categories[obj];
+            if (!obj) {
+                obj = Object.values(data.Categories).find(i => i.Name === name);
+                data.SystemSettings.TERMINAL_MENU_ITEMS[name] = obj.CategoryID;
+            }
+        }
+
         getData() {
             let self = this;
             if (self.cache)
@@ -24,6 +33,13 @@
                 Object.values(i.data.Organizations).forEach(j => {
                     j.CategoriesList = self.$linq.Enumerable().From(j.Categories).Take(6).Select(k => k.Name).ToArray().join(', ');
                 });
+                if (!i.data.SystemSettings.TERMINAL_MENU_ITEMS) {
+                    i.data.SystemSettings.TERMINAL_MENU_ITEMS = {};
+                }
+                self._setRootCategories(i.data, 'Рестораны и кафе');
+                self._setRootCategories(i.data, 'Развлечения и услуги');
+                self._setRootCategories(i.data, 'Сервисы');
+                self._setRootCategories(i.data, 'Магазины');
                 self.cache = i.data;
                 return self.cache;
             });
@@ -37,24 +53,26 @@
             let restaurantCategories = data.Categories[setting['Рестораны и кафе']].ChildIds;
             let entertainmentCategories = data.Categories[setting['Развлечения и услуги']].ChildIds;
             let serviceCategories = data.Categories[setting['Сервисы']] ? data.Categories[setting['Сервисы']].ChildIds : [];
-            let categoryID = organization.Categories[0].CategoryID;
+
+
             let type;
-            if (serviceCategories.includes(categoryID)) {
-                type = 'service';
-            } else if (entertainmentCategories.includes(categoryID)) {
-                type = 'entertainment';
-            } else if (restaurantCategories.includes(categoryID)) {
-                type = 'restaurant';
-            } else {
-                type = 'shop';
+            for (let i = 0; i < organization.Categories.length; i++) {
+                let categoryID = organization.Categories[i].CategoryID;
+                if (serviceCategories.includes(categoryID)) {
+                    return 'service';
+                } else if (entertainmentCategories.includes(categoryID)) {
+                    return 'entertainment';
+                } else if (restaurantCategories.includes(categoryID)) {
+                    return 'restaurant';
+                }
             }
-            return type;
+            return 'shop';
         }
 
         categoryGetByTypeSync(data, type) {
             let setting = data.SystemSettings.TERMINAL_MENU_ITEMS;
             if (type === 'Рестораны и кафе' || type === 'restaurant') {
-                return data.Categories[setting['Рестораны и кафе']];
+                return data.Categories[setting['Рестораны и кафе']]
             }
             if (type === 'Развлечения и услуги' || type === 'entertainment') {
                 return data.Categories[setting['Развлечения и услуги']];
